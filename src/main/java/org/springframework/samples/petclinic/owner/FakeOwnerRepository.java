982a5306d6bbfc31e14691989e8f9377847ba667
Amanda <amawai@github.com>
2018-04-09 14:22:22 -0400

    Added actual methods to FakeOwnerRepository class

--- src/main/java/org/springframework/samples/petclinic/owner/FakeOwnerRepository.java
+++ src/main/java/org/springframework/samples/petclinic/owner/FakeOwnerRepository.java
@@ -1,5 +1,44 @@
 package org.springframework.samples.petclinic.owner;
 
-public class FakeOwnerRepository {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+
+
+public class FakeOwnerRepository implements OwnerRepository{
+	private HashMap<Integer, Owner> ownerMap;
+	
+	public FakeOwnerRepository() {
+		ownerMap = new HashMap<Integer, Owner>();
+	}
+	
+	public FakeOwnerRepository(HashMap<Integer, Owner> ownerMap) {
+		this.ownerMap = ownerMap;
+	}
+	
+
+	@Override
+	public Collection<Owner> findByLastName(String lastName) {
+		List<Owner> ownerWithLastName = new ArrayList<Owner>();
+		Owner owner;
+		for (Integer id : ownerMap.keySet()) {
+			owner = ownerMap.get(id);
+			if (owner.getLastName().equals(lastName)){
+				ownerWithLastName.add(owner);
+			}
+		}
+		return ownerWithLastName;
+	}
+
+	@Override
+	public Owner findById(Integer id) {
+		return ownerMap.get(id);
+	}
+
+	@Override
+	public void save(Owner owner) {
+		ownerMap.put(owner.getId(), owner);	
+	}
 
 }
